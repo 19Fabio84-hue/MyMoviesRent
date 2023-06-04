9da2710e87b0cc5f2dca4fa00bcb460a7a3f7e86
19Fabio84-hue <piancatellif@gmail.com>
2023-06-04 19:00:26 +0200

    .

--- src/pages/refHook.js
+++ src/pages/refHook.js
@@ -1,7 +1,7 @@
 import {useState ,useEffect, useRef} from 'react'
 
 function useHover(){
-    const [hover , setHover] = useState(true)
+    const [hover , setHover] = useState(false)
     const ref = useRef(null)
 
     function Enter(){
@@ -10,19 +10,17 @@ function useHover(){
     function Leave(){
         setHover(false)
     }
-    function click(){
-        setHover(!hover)
-    }
+   
     useEffect(()=>{
         const refcurrent = ref.current
         refcurrent.addEventListener('mouseenter' ,Enter )
         refcurrent.addEventListener('mouseleave' , Leave)
-        refcurrent.addEventListener('onClick' , click)
+        
 
         return () => {
             refcurrent.removeEventListener('mouseenter' ,Enter )
         refcurrent.removeEventListener('mouseleave' , Leave)
-        refcurrent.removeEventListener('onClick' , click)
+      
         }
     },[ref])
     return [hover , ref]
